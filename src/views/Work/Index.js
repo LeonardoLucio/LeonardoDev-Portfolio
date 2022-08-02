ffa860d68473ec360165c895e3e3b4b06b93493c
LeonardoLucio <leonardo_lucio_99@hotmail.com>
2022-08-01 21:59:14 -0300

    Update

--- src/views/Work/Index.js
+++ src/views/Work/Index.js
@@ -7,6 +7,23 @@ function Work() {
         <Header />
         <F.Content>
         <F.Work>
+
+        <h2>Técnico de Operação e Serviço (N2 - INPE/CACHOEIRA PAULISTA)</h2>  
+        <h3>CENTRAL IT - Tecnologia em Negócios</h3>
+        <ul>
+        <li>Criação de Scripts para automação de processos( ShellScript, JS, Python )</li>
+        <li> Manutenção e formatação de computadores e notebooks</li>
+        <li>Mapeamento de impressoras através da rede. </li>
+        <li>Instalação e remoção de servidores no datacenter.</li>
+        <li>Configuração de rede 802.1X. </li>
+        <li>Instalação e configuração de Switchs, VOIP’s e Acess Point CISCO. </li>
+        <li>Geração de relatórios de qualidade </li>
+        <li> Cabeamento estruturado em datacenter </li>
+        <li>Auxílio na utilização de VPN com OPENVPN.  </li>
+        <li> Cabeamento estruturado em datacenter </li>
+        <li>Manutenção/configuração de Sistemas Linux (Ubunt, Debian, mint) </li>
+        </ul>
+        {/* /////////////////////////////////////////////////////////*/}
         <h2>Técnico de Suporte</h2>
         <h3>Debian Signal Comunicação Multimidia - DBNET - Provedora de internet | 06/2021 até o momento</h3>    
         <ul>
@@ -20,6 +37,7 @@ function Work() {
         <li>Manutenção/configuração de Sistemas Linux (Ubunt e Debian)</li>
         <li>Suporte a usuários nível help-desk</li>
         </ul>
+        {/* /////////////////////////////////////////////////////////*/}
         <h2>Estagiario de suporte de informatica</h2>  
         <h3>Centro Universatario Salesiano de São Paulo - UNISAL | 08/2019 ~ 09/2020</h3>
         <ul>
@@ -29,8 +47,8 @@ function Work() {
         <li>Backup e recuperação de dados</li>
         <li>Criação e manipulação de maquinas virtuais através do Citrix XenServer</li>
         <li> Suporte a usuários nível help-desk</li>
-        
         </ul>
+    
         </F.Work>
         </F.Content>
         </F.Container>
