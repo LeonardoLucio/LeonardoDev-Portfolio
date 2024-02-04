fdf41744924a2651a43ac4267965b5c77f67c255
Leonardo Lucio Batista <73317150+LeonardoLucio@users.noreply.github.com>
2024-02-04 13:44:37 -0300

    Update Index.js

--- src/views/Work/Index.js
+++ src/views/Work/Index.js
@@ -8,8 +8,32 @@ function Work() {
         <F.Content>
         <F.Work>
 
+        
+        <h2>Analista de Redes(INPE/CACHOEIRA PAULISTA)</h2>  
+        <h3>DATAINFO - Soluções em TI | 09/2023 até o momento</h3>
+        <ul>
+        <li>Administração de redes CAMPUS, datacenter fisico & virtual</li>
+        <li>Instalação e configuração de switchs L2 & L3(Cisco/Brocade/Dell/3Com)</li>
+        <li> Instalação e configuração de firewall(CISCO ASA, OPNSENSE, PFSENSE, IPTABLES)</li>
+        <li>Instalação e configuração de Hypervisor(Citrix Xen Server, Proxmox)</li>
+        <li>Criação e administração de maquinas virtuais</li>
+        <li>Instalação e configuração de VOIP cisco.</li>
+        <li>Administração de ambiente de telefonia com + 300 VOIP's (Cisco CUCM, Cisco CME)</li>
+        <li>Instalação e configuração de Access Point CISCO.</li>
+        <li>Administração de Controller WLC CISCO com + de 160 Access Point.</li>
+        <li>Suporte a VPN's site-to-site & client-to-site(VPN Cisco, OPENVPN, IPSEC)</li>
+        <li>Administração de servidores linux(ubuntu server, alma linux, rocky linux)</li>
+        <li>Monitoramento de ambiente através das ferramentas NAGIOS, ZABBIX & GRAFANA.</li>
+        <li>Segurança de host com firewall local(IPTABLES, UFW, FIREWALLD)</li>
+        <li>Automação de tarefas e scripts com ANSIBLE, CRON & TFTP.</li>
+        <li>Alteração e modificação através de gerenciamento de mudanças.</li>
+        <li>Cumprir com SLA's proposta pelo cliente resolvendo INCIDENTES, REQUISIÇÕES, EVENTOS E MUDANÇAS através da ferramente ITSM MARVAL.</li>
+        <li>Criar, alterar e revisar base de conhecimento do ambiente de redes e armazena-lo na ferramenta ITSM MARVAL.</li>
+        <li>Criação, alteração e revisão de TOPOLOGIA DE REDE atráves das ferramenta VISIO & DRAW.IO.</li>
+        </ul>
+        {/* /////////////////////////////////////////////////////////*/}
         <h2>Técnico de Suporte (INPE/CACHOEIRA PAULISTA)</h2>  
-        <h3>DATAINFO - Soluções em TI | 03/2023 até o momento</h3>
+        <h3>DATAINFO - Soluções em TI | 03/2023 ~ 09/2023</h3>
         <ul>
         <li>Criação de Scripts para automação de processos( ShellScript, JS, Python )</li>
         <li> Manutenção e formatação de computadores e notebooks</li>
